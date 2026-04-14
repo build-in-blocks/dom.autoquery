42e54769398ec81d061b7f75a1bf692b9d2a563f
Ifycode <marychord@gmail.com>
2026-04-14 03:17:28 +0100

    Fix: Test 2

--- src/index.ts
+++ src/index.ts
@@ -12,20 +12,6 @@ export const DOMautoquery = {
       console.log('Active Device:', activeDevice);
     });
 
-    // const { dummyConsole } = await import(
-    //     /* webpackChunkName: "dummy-console" */
-    //     /* webpackMode: "lazy" */
-    //     './dom.autoquery/console.js'
-    // );
-    // dummyConsole();
-
-    //     const { dummyConsole2 } = await import(
-    //   /* webpackChunkName: "another.dummy" */
-    //     /* webpackMode: "lazy" */
-    //   './dom.autoquery/another.js'
-    // );
-    // dummyConsole2();
-
     const { dummyConsole } = await import(
       /* webpackChunkName: "dummy.console" */
       /* webpackExclude: /\.d\.ts$/ */
@@ -41,3 +27,43 @@ export const DOMautoquery = {
     dummyConsole2();
   },
 };
+
+// export const DOMautoquery = {
+//   devices: async (_devices: DOMautoqueryDevices) => {
+//     //-----------------------
+//     // Use breakpoint watcher
+//     //-----------------------
+//     breakpointsWatcher(_devices, (activeDevice: ActiveDevice) => {
+//       //   console.clear();
+//       console.log('Active Device:', activeDevice);
+//     });
+
+//     // const { dummyConsole } = await import(
+//     //     /* webpackChunkName: "dummy-console" */
+//     //     /* webpackMode: "lazy" */
+//     //     './dom.autoquery/console.js'
+//     // );
+//     // dummyConsole();
+
+//     //     const { dummyConsole2 } = await import(
+//     //   /* webpackChunkName: "another.dummy" */
+//     //     /* webpackMode: "lazy" */
+//     //   './dom.autoquery/another.js'
+//     // );
+//     // dummyConsole2();
+
+//     const { dummyConsole } = await import(
+//       /* webpackChunkName: "dummy.console" */
+//       /* webpackExclude: /\.d\.ts$/ */
+//       './dummy/console'
+//     );
+//     dummyConsole();
+
+//     const { dummyConsole2 } = await import(
+//       /* webpackChunkName: "another.dummy" */
+//       /* webpackExclude: /\.d\.ts$/ */
+//       './dummy/another'
+//     );
+//     dummyConsole2();
+//   },
+// };
