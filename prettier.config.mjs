1720a32fbee711c0b597acb8fd2f624f776b58e9
Mary @Ifycode <marychord@gmail.com>
2026-04-16 13:56:05 +0100

    Revert "Add: All dev setup files"
    This reverts commit c9db586c139f83c423056cc6f47a3feb7ecbb66b.

--- prettier.config.mjs
+++
@@ -1,8 +0,0 @@
-import basePrettier from '@build-in-blocks/dev.setup/prettier';
-
-export default {
-  //-------------------------------
-  // USE OUR PRECONFIGURED SETTINGS
-  //-------------------------------
-  ...basePrettier,
-};
